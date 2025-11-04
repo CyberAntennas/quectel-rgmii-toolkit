37ead8671d06699de8d3916e343a2a06d49019c7
Karim O. <146198118+kmoz000@users.noreply.github.com>
2025-11-04 03:32:41 +0100

    Change GITUSER from 'iamromulan' to 'CyberAntennas'

--- RMxxx_rgmii_toolkit.sh
+++ RMxxx_rgmii_toolkit.sh
@@ -2,7 +2,7 @@
 
 # Define toolkit paths
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/opt/bin:/opt/sbin:/usrdata/root/bin
-GITUSER="iamromulan"
+GITUSER="CyberAntennas"
 REPONAME="quectel-rgmii-toolkit"
 GITTREE="SDXLEMUR"
 GITMAINTREE="SDXLEMUR"
